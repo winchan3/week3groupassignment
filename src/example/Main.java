4707c0b4a1344251fb0e41ee9c2cc7a4cef8e3d7
charles <charles.feng@mail.utoronto.ca>
2023-10-01 17:32:57 -0400

    Charles Feng's api call example code

---
+++ src/example/Main.java
@@ -0,0 +1,9 @@
+package example;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(new apiCall().exampleCall());
+    }
+}
